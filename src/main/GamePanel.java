dce1dd2aa7220ffdceaf20f8c61688f93e7976f1
ShatunovaKateryna <kateshatunova@gmail.com>
2024-11-14 21:35:44 +0200

    v2

---
+++ src/main/GamePanel.java
@@ -0,0 +1,54 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    final int originalTitlesSize = 16;
+    final int scale = 3;
+    final int titleSize = originalTitlesSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = titleSize * maxScreenCol;
+    final int screenHeight = titleSize * maxScreenRow;
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+            //System.out.println("The game loop is running");
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(100, 100, titleSize, titleSize);
+        g2.dispose();
+    }
+}
